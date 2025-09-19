818f0d40faf35b549d94a548aac00004b411d985
chandu-569 <chanduchintakayala2@gmail.com>
2025-09-19 14:19:03 +0530

    project1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./Components/NavBar"
+import SignUp from "./Components/SignUp"
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <SignUp />
+    </>
+  )
+}
+
+export default App
